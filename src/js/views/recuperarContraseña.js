392269e0e45e6e43b9ebdad0669297c730fbbff7
LeandroMatonte <leandromatonte99@gmail.com>
2021-06-10 13:56:45 +0000

    vista recperar contraseña

--- src/js/views/recuperarContraseña.js
+++ src/js/views/recuperarContraseña.js
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
 
 export const RecuperarContraseña = () => {
+	const { store, actions } = useContext(Context);
+	const [email, setEmail] = useState("");
+	const [enviado, setEnviado] = useState(false);
 	return (
-		<div className="container mt-5">
+		<div className="container my-auto">
 			<div className="row justify-content-center">
-				<div className="col-6 shadow" id="borde">
-					<h1 className="ml-3 mt-2">Recuperar contraseña</h1>
-					<form>
-						<div className="mb-3 mt-4" id="ingresarEmail">
-							<label htmlFor="exampleInputEmail1" className="form-label">
-								Email
-							</label>
-							<input
-								type="email"
-								className="form-control"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-							/>
-							<div id="email1" className="form-text">
-								Le enviaremos un correo de confirmación
-							</div>
-						</div>
-						<div>
-							<button type="submit" className="btn btn-primary" id="btnAceptar">
-								Aceptar
-							</button>
-						</div>
-					</form>
+				<div className="col-sm-12 col-md-6">
+					<div className="p-5 borde rounded shadow">
+						{!enviado ? (
+							<form
+								onSubmit={e => {
+									e.preventDefault();
+									actions.recuperarPass(email);
+									setEnviado(true);
+								}}>
+								<h2>Recuperar contraseña</h2>
+								<div className="form-group">
+									<label htmlFor="email" className="form-label">
+										Email
+									</label>
+									<input
+										onChange={e => {
+											e.target.value.trim() != "" ? setEmail(e.target.value) : setEmail("");
+										}}
+										type="email"
+										className="form-control"
+										id="email"
+										value={email}
+										aria-describedby="emailHelp"
+									/>
+
+									<small id="emailHelp" className="form-text text-muted">
+										Le enviaremos un correo de confirmación.
+									</small>
+								</div>
+								<div className="d-flex justify-content-center">
+									<button type="submit" className="btn boton">
+										Aceptar
+									</button>
+								</div>
+							</form>
+						) : (
+							<h2>Le enviaremos un correo de confirmación.</h2>
+						)}
+					</div>
 				</div>
 			</div>
 		</div>
