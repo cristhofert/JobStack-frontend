a8aa5f2bfd7de2ac5febf63e469ab2b40229aa7a
melagalaretto <moonsea1711@gmail.com>
2021-06-17 16:54:16 +0000

    homepage

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -21,7 +21,7 @@ export const Login = () => {
 		}
 	};
 
-	const responseGoogle = async responseG => {
+	/* const responseGoogle = async responseG => {
 		const respuesta = await actions.loginGoogle(responseG.profileObj.email);
 		if (respuesta) {
 			history.push("/");
@@ -29,7 +29,7 @@ export const Login = () => {
 			setError(true);
 		}
 		console.log(respuesta);
-	};
+	}; */
 	return (
 		<div className="container my-auto">
 			<div className="row">
@@ -93,12 +93,12 @@ export const Login = () => {
 					<div className="mt-3 d-flex justify-content-center align-items-center">
 						<h2>Otros medios de ingreso</h2>
 					</div>
-					<GoogleLogin
+					{/* <GoogleLogin
 						clientId="1012985398043-k3re8g9vs1ahlk5nug7lvm3o0e2lnl6b.apps.googleusercontent.com"
 						onSuccess={responseGoogle}
 						onFailure={responseGoogle}
 						cookiePolicy={"single_host_origin"}
-					/>
+					/> */}
 				</div>
 			</div>
 		</div>
